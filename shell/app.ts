083e7c15804727ccbaf3aa8501fe4c1c4419c7e8
Dheerendra Singh <dheerendra.mcs16.du@gmail.com>
2024-08-11 10:33:11 +0530

    Microfront end with multiple framework

--- shell/app.ts
+++ shell/app.ts
@@ -1,9 +1,12 @@
-import { loadRemoteModule } from '@softarc/native-federation';
 import * as rxjs from 'rxjs';
 import * as sharedLib  from 'shared-lib';
 
+import { loadRemoteModule } from '@softarc/native-federation';
+
 const container = document.getElementById('container');
 const flightsLink = document.getElementById('flights');
+const adminLink = document.getElementById('admin');
+const reactLink = document.getElementById('react');
 const homeLink = document.getElementById('home');
 
 function removeFirstChild() {
@@ -34,6 +37,35 @@ function displayWelcomeMessage() {
         removeFirstChild();       
         container.appendChild(elm);
     });
+
+    rxjs.fromEvent(adminLink, 'click').subscribe(async _ => {
+      const elm1 = document.createElement('app-root');
+      removeFirstChild();
+      container.appendChild(elm1);
+        
+        const module = await loadRemoteModule({
+            exposedModule: './Component',
+            remoteEntry: 'http://localhost:4201/remoteEntry.json',
+            remoteName: 'admin'
+        });
+
+      const elm = document.createElement('app-component');
+      removeFirstChild();
+      container.appendChild(elm);
+    });
+
+    rxjs.fromEvent(reactLink, 'click').subscribe(async _ => {
+          
+          const module = await loadRemoteModule({
+              exposedModule: './component-react',
+              remoteEntry: 'http://localhost:3101/remoteEntry.json',
+              remoteName: 'react-mfe1'
+          });
+  
+        const elm = document.createElement('react-mf1');
+        removeFirstChild();
+        container.appendChild(elm);
+      });
 
     rxjs.fromEvent(homeLink, 'click').subscribe(_ => {
         displayWelcomeMessage();
--- shell/main.ts
+++ shell/main.ts
@@ -1,3 +1,5 @@
+import 'zone.js';
+
 import { initFederation } from '@softarc/native-federation';
 
 (async () => {
